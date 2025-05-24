e3c216dfb68ac20ef4e8c33e579710dfa3d13c3d
zemblov <zemblov@yahoo.com>
2025-05-24 02:43:25 -0400

    Create roleMiddleware.js

---
+++ middleware/roleMiddleware.js
@@ -0,0 +1,6 @@
+const authorize = (role) => (req, res, next) => {
+  if (req.user.role !== role) return res.status(403).json({ error: 'Access denied' });
+  next();
+};
+
+module.exports = authorize;
